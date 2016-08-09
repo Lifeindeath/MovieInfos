ff94c60f1a6d8bc8b907aae617ec3ab7452e9241
lifeindeath <inca.chelsea@gmail.com>
2016-08-09 21:42:33 +0200

    [Backend]Parser for filmup done

--- bin/app.py
+++ bin/app.py
@@ -17,8 +17,12 @@ class Index(object):
     def POST(self):
         form = web.input(movie_url=None)
         #greeting =  "%s, %s" % (form.greet, form.name)
-        image_url = scraper_0.get_info_from_filmup(form.movie_url)
-        return render.index(image_url = image_url)
+        field_dict = scraper_0.get_info_from_filmup(form.movie_url)
+        print field_dict
+        return render.index(title = field_dict.get('Titolo originale:'), \
+        					genre = field_dict.get('Genere:'), \
+        					year = field_dict.get('Anno:'),    \
+        					cast = field_dict.get('Cast:'))
 
 if __name__ == "__main__":
     app.run()
--- bin/scraper_0.py
+++ bin/scraper_0.py
@@ -8,25 +8,57 @@ Created on Wed Aug  3 21:37:35 2016
 from lxml import html
 import requests
 from urllib import urlretrieve 
+import re
+
+def get_text(el):
+    if isinstance(el, basestring):
+        return el
+    else:
+        return el.xpath('text()')[0]
+
+def is_field(string):
+    str_is_field = r'.*:\xa0'
+    if len(re.findall(str_is_field,string))>0:        
+        return True
+    else:
+        return False
+
+def merge_infos(main_info, plot):    
+    field_name = r'.*:'
+    fields = []
+    values = []
+    value = ''
+    end = 0
+    for i in main_info:
+        if is_field(i):
+            values += [value]
+            value = ''
+            fields += [re.findall(field_name,i)[0]]            
+        else:
+            value += i
+    fields += [plot[0]]
+    values += [plot[1]]
+    
+    return dict(zip(fields,values[1:]))
 
 def get_info_from_filmup(film_url):
 	page = requests.get(film_url)
 	tree = html.fromstring(page.content)
 
 	#Fetch the infos as a list:
-	info = tree.xpath('//div[@id="container"]/table/tr/td/div/table/tr/td/table/tr/td/table/tr/td/font/text()')
+	info = tree.xpath('//div[@id="container"]/table/tr/td/div/table/tr/td/table/tr/td/table/tr/td/font/node()')
+	info = [get_text(i) for i in info]
 	plot = tree.xpath('//div[@id="container"]/table/tr/td/div/table/tr/td/table/tr/td/font/text()')
 	image = tree.xpath('//div[@id="container"]/table/tr/td/div/table/tr/td/table/tr/td/table/form/tr/td/a[@class="filmup"]/@href')
-
-	#print(image[0])
+    
 	#Fetch large image url
 	image_page = requests.get('http://filmup.leonardo.it/'+image[0])
 	tree = html.fromstring(image_page.content)
 
 	image_big = tree.xpath('//div[@id="container"]/table/tr/td/div/div/img/@src')
-		
+
 	#Download image in local folder
 	urlretrieve('http://filmup.leonardo.it'+image_big[0], "images/loc.jpg")
 	#print(image downloaded)
-	return 'http://filmup.leonardo.it'+image_big[0]
-
+	res = merge_infos(info, plot)
+	return res
